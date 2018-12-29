2f09ca257b7f521a57454a260f8e91b764c971c8
Sami Jawhar <sami.n.jawhar@gmail.com>
2018-12-29 21:02:38 +0800

    Added coins and deathzone

--- super-sparty-bros/Assets/Scripts/Coin.cs
+++ super-sparty-bros/Assets/Scripts/Coin.cs
@@ -4,30 +4,33 @@ using System.Collections;
 public class Coin : MonoBehaviour {
 
 	public int coinValue = 1;
-	public bool taken = false;
 	public GameObject explosion;
+
+	bool taken = false;
 
 	// if the player touches the coin, it has not already been taken, and the player can move (not dead or victory)
 	// then take the coin
 	void OnTriggerEnter2D (Collider2D other)
 	{
-		if ((other.tag == "Player" ) && (!taken) && (other.gameObject.GetComponent<CharacterController2D>().playerCanMove))
+		if (taken || other.tag != "Player")
 		{
-			// mark as taken so doesn't get taken multiple times
-			taken=true;
-
-			// if explosion prefab is provide, then instantiate it
-			if (explosion)
-			{
-				Instantiate(explosion,transform.position,transform.rotation);
-			}
-
-			// do the player collect coin thing
-			other.gameObject.GetComponent<CharacterController2D>().CollectCoin(coinValue);
-
-			// destroy the coin
-			DestroyObject(this.gameObject);
+			return;
 		}
+		CharacterController2D controller = other.gameObject.GetComponent<CharacterController2D>();
+		if (!controller || !controller.playerCanMove) {
+			return;
+		}
+
+		taken = true;
+		controller.CollectCoin(coinValue);
+
+		// if explosion prefab is provide, then instantiate it
+		if (explosion)
+		{
+			Instantiate(explosion,transform.position,transform.rotation);
+		}
+		// destroy the coin
+		Destroy(this.gameObject);
 	}
 
 }
